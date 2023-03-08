55cc664e88b175d19bd9a23bbf2268d3bc86d888
Cari Gostic <cgostic@sonomatech.com>
2023-03-08 11:30:50 -0800

    Correct lin reg formula formatting to 2 decimals

--- R/scatterServer.R
+++ R/scatterServer.R
@@ -232,9 +232,9 @@ scatterServer <- function(id,
             lm_eqn <- function(df){
               m <- stats::lm(stats::as.formula(paste(param2_val, '~', param1_val)), data = df)
               eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)^2~"="~r2,
-                               list(a = format(unname(stats::coef(m)[1]), digits = 2),
-                                    b = format(unname(stats::coef(m)[2]), digits = 2),
-                                    r2 = format(summary(m)$r.squared, digits = 3)))
+                               list(a = format(round(unname(stats::coef(m)[1]), 2), nsmall = 2),
+                                    b = format(round(unname(stats::coef(m)[2]),2), nsmall = 2),
+                                    r2 = format(round(summary(m)$r.squared,2), nsmall= 2)))
               as.character(as.expression(eq));
             }
             plot <- plot +
